553b1d8465e234157fa2afc4920d9960c59d1d71
hemal hassan <hemalhassan8@gmail.com>
2020-10-30 22:09:53 +0600

    blood group input field

---
+++ LAB_TASK 3.1php/C_TASK_3_INPUT_FIELD/Blood group.php
@@ -0,0 +1,39 @@
+
+<?php 
+if(isset($_POST['submit'])){
+	$Bloodgroup = $_POST['bloodgroup'];
+	echo $Bloodgroup;
+}
+
+
+
+ ?>
+
+<html>
+<head>
+	<title>Blood group</title>
+</head>
+<body>
+	<form method="post">
+		<fieldset>
+			<legend>
+				Blood group
+			</legend>
+			<select name="blood group"></select>
+			<option value="<?php 
+if(isset($_POST['submit'])){
+	$Bloodgroup = $_POST['bloodgroup'];
+	echo $Bloodgroup;
+}
+
+
+
+ ?>"></option><hr/>
+
+			<input type="submit" name="submit" value="submit">
+
+		</fieldset>
+	</form>
+
+</body>
+</html>
