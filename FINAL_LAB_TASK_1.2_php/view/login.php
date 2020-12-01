5df7dcb152b5f5f184d63def5b3ce5b303050960
hemal hassan <hemalhassan8@gmail.com>
2020-12-01 09:26:21 +0600

    last commit

---
+++ FINAL_LAB_TASK_1.2_php/view/login.php
@@ -0,0 +1,51 @@
+<?php
+
+	if(isset($_GET["msg"])){
+		
+		if($_GET["msg"] == "invalid_user"){
+			echo "username/password not valid...";
+		}
+
+		if($_GET["msg"] == "null_username"){
+			echo "Username field is empty...";
+		}
+
+		if($_GET["msg"] == "null_password"){
+			echo "Password field is empty...";
+		}
+	}
+
+    if(isset($_COOKIE["isLoggedIn"]))
+    {
+		header("Location: home.php");
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ADMIN Login</title>
+</head>
+<body>
+	<form method="post" action="../php/logincheck.php">
+		<fieldset>
+			<legend>ADMIN LOGIN</legend>
+			<table>
+				<tr>
+					<td>Username</td>
+					<td><input type="text" name="username" ></td>
+				</tr>
+				<tr>
+					<td>Password</td>
+					<td><input type="password" name="password"></td>
+				</tr>
+				
+				<tr>
+					<td></td>
+					<td><input type="submit" name="submit" value="Login"></td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+</body>
+</html>
